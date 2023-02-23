a221d5350640afaeba3eb883895c8fc09f92b25e
lkuca <120181604+lkuca@users.noreply.github.com>
2023-02-23 11:28:19 +0200

    Update omamoodul.py

--- sõnastik/omamoodul.py
+++ sõnastik/omamoodul.py
@@ -1,4 +1,4 @@
-
+from random import *
 def Loe_failist(fail:str)->list:
     """
     """
@@ -8,8 +8,25 @@ def Loe_failist(fail:str)->list:
         jarjend.append(rida.strip())
     f.close()
     return jarjend
-#def Kirjuta_failisse(fail:str,jarjend:list):
-#    f=open(fail,"w",encoding="utf-8-sig")
-#    for line in jarjend:
-#        f.write(line+"\n")
-#    f.close()
+def Kirjuta_failisse(fail:str,jarjend:list):
+    f=open(fail,"a",encoding="utf-8-sig")
+    for line in jarjend:
+        f.write(line+"\n")
+    f.close()
+def svabra_gaming(jarjend:list):
+        x=input("vana sõna")
+        y=input("uus sõna")
+        if x in jarjend:
+            ind=jarjend.index(x)
+            jarjend.remove(x)
+            jarjend.insert(ind,y)
+def game(jarjend:list):
+    x=random
+    y=input()
+    while x in jarjend:
+            ind=jarjend.index(x)
+            if x==y:
+                print("õige")
+                continue
+            #else
+
